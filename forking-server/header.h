2d571ca0d6a7eb386da744727e8d5239ffe1d60c
Paul Serbanescu <paulserbanescu3@gmail.com>
2023-01-17 00:36:19 -0500

    add color codes

--- forking-server/header.h
+++ forking-server/header.h
@@ -19,4 +19,12 @@
 #define SIG_CLIENT_EXIT "SIG_CLIENT_EXIT"
 #define USR_SIZE 255
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
 #endif
